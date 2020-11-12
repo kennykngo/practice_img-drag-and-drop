2bb21a1b6a94119cb32d3a7df03d6003a942b48f
Kenny Ngo <kenny.ngo.design@gmail.com>
2020-11-11 23:44:21 -0800

    finished

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
 document.querySelectorAll(".drop-zone__input").forEach((inputElement) => {
   const dropZoneElement = inputElement.closest(".drop-zone");
 
+  dropZoneElement.addEventListener("click", (e) => {
+    inputElement.click();
+  });
+
+  inputElement.addEventListener("change", (e) => {
+    if (inputElement.files.length) {
+      updateThumbnail(dropZoneElement, inputElement.files[0]);
+    }
+  });
+
   dropZoneElement.addEventListener("dragover", (e) => {
     e.preventDefault();
     dropZoneElement.classList.add("drop-zone--over");
@@ -26,8 +36,39 @@ document.querySelectorAll(".drop-zone__input").forEach((inputElement) => {
     dropZoneElement.classList.remove("drop-zone--over");
   });
 });
+/*
+ * @param {HTMLElement} dropZoneElement
+ * @param {name}
+ */
 
 function updateThumbnail(dropZoneElement, file) {
-  console.log(dropZoneElement);
-  console.log(file);
+  let thumbnailElement = dropZoneElement.querySelector(".drop-zone__thumb");
+
+  if (dropZoneElement.querySelector(".drop-zone__prompt")) {
+    dropZoneElement.querySelector(".drop-zone__prompt").remove();
+  }
+
+  // First time - there is not thumbnail element, so making
+  if (!thumbnailElement) {
+    thumbnailElement = document.createElement("div");
+    thumbnailElement.classList.add("drop-zone__thumb");
+    dropZoneElement.appendChild(thumbnailElement);
+  }
+
+  thumbnailElement.dataset.label = file.name;
+
+  // Show thumbnail for image files
+  if (file.type.startsWith("image/")) {
+    const reader = new FileReader();
+
+    reader.readAsDataURL(file);
+
+    // async call
+    reader.onload = () => {
+      // reader.result is container the base 64 image
+      thumbnailElement.style.backgroundImage = `url("${reader.result}")`;
+    };
+  } else {
+    thumbnailElement.style.backgroundImage = null;
+  }
 }
